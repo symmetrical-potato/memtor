6a714452b57f600e9799843b9aeb3efb98911915
Mikhail Logachev <miha.loga@gmail.com>
2017-08-26 23:15:07 +0300

    test demo

--- app/application.py
+++ app/application.py
@@ -9,9 +9,33 @@ def index():
 
 @app.route('/test')
 def test():
-    return render_template('info.html',
-                           name="Testers community",
-                           description="Community for true tester of everything and everywhere!")
+
+    data = {
+        'name': "Testers community",
+        'description': 'Community for true tester of everything and everywhere!',
+        'link': 'example',
+        'median_likes': {
+            'month': 30,
+            'two_weeks': 39,
+            'one_week': 45
+        },
+        'median_comments': {
+            'month': 12,
+            'two_weeks': '19',
+            'one_week': 22
+        },
+        'median_reposts': {
+            'month': '22',
+            'two_weeks': '18',
+            'one_week': '24'
+        },
+        'general': {
+            'posts_freq': '41/week',
+            'avg_post_len': '244'
+        }
+    }
+
+    return render_template('info.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
