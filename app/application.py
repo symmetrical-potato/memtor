30aa502f74af3c8fc3514ee4cdd3db9d752595d9
Mikhail Logachev <miha.loga@gmail.com>
2017-08-26 21:53:10 +0300

    improved css with loader, info page

--- app/application.py
+++ app/application.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('info.html',
+                           name="Testers community",
+                           description="Community for true tester of everything and everywhere!")
+
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
-    # print(app.url_map)
---
+++ app/static/index_3_10.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('.smooth').click( function(e) {
+        var rd = $(this).attr('href');
+        e.preventDefault();
+
+        $('body').fadeOut(400, function() {
+            document.location.href = rd;
+        })
+    })
+
+    // $('.input-group-btn').click( function() {
+    //     $(this).children('button').html('<div class="loader"></div>');
+    //
+    //     console.log('111');
+    //     console.log(showTestView);
+    //
+    //     setTimeout(500, function () {
+    //         $('body').fadeOut(400, function () {
+    //             console.log(document.location.href);
+    //             window.location.href = 'http://localhost:5000/test';
+    //         });
+    //     });
+    // });
+
+    // $('#smooth').click(function (e) {
+    //     var rd = $(this).attr('href');
+    //     e.preventDefault();
+    //
+    // })
+});
