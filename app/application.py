bd1449f8dfbf5d25740787febe3e52580e06553a
Mikhail Logachev <miha.loga@gmail.com>
2017-08-27 02:12:43 +0300

    word cloud for test

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_file
+import json
 
 app = Flask(__name__)
 
@@ -7,10 +8,32 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/stuff')
+def stuff():
+    with open('../topics/topics.json') as json_data:
+        data = json.load(json_data)
+
+    for d in data:
+        print(d)
+
+    return '1'
+
 @app.route('/test')
 def test():
 
-    data = {
+    clouds = []
+
+    with open('../topics/topics.json') as json_data:
+        data = json.load(json_data)
+
+        for i in range(5, 0, -1):
+            for j in range(len(data[5 - i])):
+                clouds.append([i * 10, data[5 - i][j]])
+
+    print(clouds)
+
+    test_data = {
         'name': "Testers community",
         'description': 'Community for true tester of everything and everywhere!',
         'link': 'example',
@@ -32,10 +55,11 @@ def test():
         'general': {
             'posts_freq': '41/week',
             'avg_post_len': '244'
-        }
+        },
+        'clouds': clouds
     }
 
-    return render_template('info.html', data=data)
+    return render_template('info.html', data=test_data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
