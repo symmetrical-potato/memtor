84dd9404e3dec9339335b2f7c5344cf1bc94532a
Mikhail Logachev <miha.loga@gmail.com>
2017-08-26 19:14:13 +0300

    minor tweaks

--- app/application.py
+++ app/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/static/<string:file_name>')
+def serve_static(file_name):
+    app.logger.info(file_name)
+    return send_file('static/' + file_name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
