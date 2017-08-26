55e2440abe016721424c63a757ce9086def5a3d4
Mikhail Logachev <miha.loga@gmail.com>
2017-08-26 19:08:41 +0300

    styles and input to app

--- app/application.py
+++ app/application.py
@@ -8,5 +8,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='localhost', port=5000)
     # print(app.url_map)
