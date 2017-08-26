db3a109a15a2f0b9a44edb4a8a9b454f3c40038f
Mikhail Logachev <miha.loga@gmail.com>
2017-08-26 20:03:19 +0300

    versioned css

--- app/application.py
+++ app/application.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/static/<string:file_name>')
-def serve_static(file_name):
-    app.logger.info(file_name)
-    return send_file('static/' + file_name)
-
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
     # print(app.url_map)
